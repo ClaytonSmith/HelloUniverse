ddbbadb47c23a4edd4f63d8433201f8ba7a93eea
ClaytonSmith <cks_jr@yahoo.com>
2015-01-05 13:43:17 -0500

    First commit

---
+++ HelloUniverse.cpp
@@ -0,0 +1,17 @@
+/*
+ * Written by: Clayton Smith
+ *
+ * Created of: 1-5-2015 (dd-mm-yyyy)
+ *
+ * File: HelloUniverse.cpp
+ *
+ */
+
+#include <iostream>
+
+int main( int argc, char *argv[] ){
+  
+  std::cout << "Hello Universe!" << std::endl;
+
+  return 0;
+}
