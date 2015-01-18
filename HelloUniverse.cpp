a134b0f3dcdb3dae6299d7a8b829d0ee40976953
Clayton Smith <Cks_jr@yahoo.com>
2015-01-18 18:40:10 -0500

    Update HelloUniverse.cpp

--- HelloUniverse.cpp
+++ HelloUniverse.cpp
@@ -10,8 +10,8 @@
 #include <iostream>
 
 int main( int argc, char *argv[] ){
-  
-  std::cout << "Hello Universe!" << std::endl;
 
+  std::cout << "Hello Universe!" << std::endl;
+  
   return 0;
 }
